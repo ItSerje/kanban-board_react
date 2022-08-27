fd987adca920c88106dc3c2d369aa745138420f8
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-27 17:22:29 +0600

    replace Card test jsx with bootstrap componenets

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import BootstrapCard from 'react-bootstrap/Card'
 import './style.css'
 
-const Card = ({ card }) => {
-  const { id, author, text } = card
+const Card2 = ({ card, addCardHandler }) => {
+  const { author, text } = card
+
   return (
-    <article className='card'>
-      <h1>Author: {author}</h1>
-      <p>{text}</p>
-    </article>
+    <BootstrapCard style={{ width: '18rem' }}>
+      {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
+      <BootstrapCard.Body>
+        <BootstrapCard.Title>{text}</BootstrapCard.Title>
+        <BootstrapCard.Text>{author}</BootstrapCard.Text>
+        <Button variant='primary' onClick={addCardHandler}>
+          Add card
+        </Button>
+      </BootstrapCard.Body>
+    </BootstrapCard>
   )
 }
 
-export default Card
+export default Card2
