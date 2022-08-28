d001ee763263c861cec6961b8b8b3a1218aebbcd
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-29 02:03:32 +0600

    update Card structure, remove inline style

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,24 +5,28 @@ const Card = ({ card, openCardHandler }) => {
   const { commentsCounter, title } = card;
 
   return (
-    <BootstrapCard style={{ width: '18rem' }} onClick={openCardHandler}>
+    <BootstrapCard onClick={openCardHandler}>
       {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
+      <span className='card-edit-icon'></span>
       <BootstrapCard.Body>
         <BootstrapCard.Title>{title}</BootstrapCard.Title>
         {/* <BootstrapCard.Text>{author}</BootstrapCard.Text> */}
         {/* <Button variant='primary' onClick={addCardHandler}>
           Add card
         </Button> */}
+      </BootstrapCard.Body>
+
+      <BootstrapCard.Footer>
+        {/* <small className='text-muted'>Last updated 3 mins ago</small> */}
 
         {/* maybe the below is worth to be a separate component? */}
         {/* basides there is an option to use react-bootstrap Badge and/or ListGroup */}
-        <div className='card-badges'>
-          <div className='card-badge' title='Comments'>
-            <span className='card-badge-icon'></span>
-            <span className='card-badge-text'>{commentsCounter}</span>
-          </div>
+
+        <div className='card-badge' title='Comments'>
+          <span className='card-badge-icon'></span>
+          <span className='card-badge-text'>{commentsCounter}</span>
         </div>
-      </BootstrapCard.Body>
+      </BootstrapCard.Footer>
     </BootstrapCard>
   );
 };
