2529ed0bc34bd9c84e800762ccf5d59abf9d9447
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-29 16:13:07 +0600

    add LoginModal and its basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import CardsList from './components/cardsList/CardsList';
+import LoginModal from './components/LoginModal/LoginModal';
 import testJson from './test.json';
 
 function App() {
+  const [modalShow, setModalShow] = useState(true);
+  const closeModalHandler = () => setModalShow(false);
+  const loginHandler = () => console.log('login');
+
   return (
     <section className='dashboard'>
+      <LoginModal
+        modalShow={modalShow}
+        closeModalHandler={closeModalHandler}
+        loginHandler={loginHandler}
+      />
       <CardsList cards={testJson} />
       <CardsList cards={testJson} />
       <CardsList cards={testJson} />
