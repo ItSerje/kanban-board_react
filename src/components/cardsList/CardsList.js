125e5f60e9839b214edd7adf6e1e496fd05f7a72
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-29 09:27:02 +0600

    update CardList structure and styles as per reqs

--- src/components/cardsList/CardsList.js
+++ src/components/cardsList/CardsList.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import Card from '../card/Card';
+import BootstrapCard from 'react-bootstrap/Card';
 import './style.css';
+import Card from '../card/Card';
 
 const CardsList = ({ cards }) => {
   return (
-    <div className='cards'>
-      {cards.map((card) => (
-        <>
-          {console.log(card)}
-          <Card card={card} key={card.id} />
-        </>
-      ))}
+    <div className='cards-list-wrapper'>
+      <BootstrapCard className='cards-list'>
+        <BootstrapCard.Header className='cards-list-header'>
+          Column name
+        </BootstrapCard.Header>
+        <BootstrapCard.Body className='cards-list-body'>
+          {cards.map((card) => (
+            <>
+              {console.log(card)}
+              <Card card={card} key={card.id} />
+            </>
+          ))}
+        </BootstrapCard.Body>
+        <BootstrapCard.Footer className='cards-list-footer'>
+          + Add a card
+        </BootstrapCard.Footer>
+      </BootstrapCard>
     </div>
   );
 };
