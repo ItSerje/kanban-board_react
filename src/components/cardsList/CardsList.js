6bb78bf56c96ee51b30b9da90c583f37c752527f
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-30 03:45:11 +0600

    enable name change, based on global context

--- src/components/cardsList/CardsList.js
+++ src/components/cardsList/CardsList.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 import BootstrapCard from 'react-bootstrap/Card';
+import BootstrapForm from 'react-bootstrap/Form';
 import './style.css';
 import Card from '../card/Card';
+import { useDashboardContext } from '../../context/dashboard_context';
 
-const CardsList = ({ cards }) => {
+const CardsList = ({ cards, name, columnId }) => {
+  const [isNameEditMode, setNameEditMode] = useState(false);
+  const { dashboard, setDashboard } = useDashboardContext();
+  const [inputValue, setInputValue] = useState(name);
+
   return (
-    <div className='cards-list-wrapper'>
-      <BootstrapCard bsPrefix='cards-list'>
-        <BootstrapCard.Header bsPrefix='cards-list-header'>
-          Column name
-        </BootstrapCard.Header>
-        <BootstrapCard.Body bsPrefix='cards-list-body'>
-          {cards.map((card) => (
-            <Card card={card} key={card.id} />
-          ))}
-        </BootstrapCard.Body>
-        <BootstrapCard.Footer bsPrefix='cards-list-footer'>
-          + Add a card
-        </BootstrapCard.Footer>
-      </BootstrapCard>
-    </div>
+    <BootstrapCard bsPrefix='cards-list'>
+      <BootstrapCard.Header
+        bsPrefix='cards-list-header'
+        onClick={() => {
+          setNameEditMode(true);
+        }}
+      >
+        {isNameEditMode ? (
+          <BootstrapForm>
+            <BootstrapForm.Group
+              className='mb-3'
+              controlId='exampleForm.ControlTextarea1'
+            >
+              <BootstrapForm.Control
+                as='textarea'
+                rows={1}
+                autoFocus
+                value={inputValue}
+                onChange={(e) => {
+                  setInputValue(e.target.value);
+                }}
+                onBlur={(e) => {
+                  const newDashboard = { ...dashboard };
+                  newDashboard.columns[columnId].name = e.target.value;
+                  setDashboard(newDashboard);
+                  setNameEditMode(false);
+                }}
+              />
+            </BootstrapForm.Group>
+          </BootstrapForm>
+        ) : (
+          name
+        )}
+      </BootstrapCard.Header>
+      <BootstrapCard.Body bsPrefix='cards-list-body'>
+        {Object.keys(cards).map((key) => {
+          const card = cards[key];
+          return <Card card={card} key={card.id} />;
+        })}
+      </BootstrapCard.Body>
+      <BootstrapCard.Footer bsPrefix='cards-list-footer'>
+        + Add a card
+      </BootstrapCard.Footer>
+    </BootstrapCard>
   );
 };
 
