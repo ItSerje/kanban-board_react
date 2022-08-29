701069020344ad3048693f618c369ae5969abefd
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-30 03:41:16 +0600

    refactor as per new json data, import context

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,38 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import CardsList from './components/cardsList/CardsList';
+import CardsListContainer from './components/cardsList/CardsListContainer';
 import LoginModal from './components/LoginModal/LoginModal';
-import testJson from './test.json';
+import { useDashboardContext } from './context/dashboard_context';
 
 function App() {
   const [modalShow, setModalShow] = useState(true);
   const closeModalHandler = () => setModalShow(false);
   const loginHandler = () => console.log('login');
 
+  const { dashboard } = useDashboardContext();
+
   return (
     <section className='dashboard'>
-      <LoginModal
-        modalShow={modalShow}
-        closeModalHandler={closeModalHandler}
-        loginHandler={loginHandler}
-      />
-      <CardsList cards={testJson} />
-      <CardsList cards={testJson} />
-      <CardsList cards={testJson} />
-      <CardsList cards={testJson} />
+      <>
+        <LoginModal
+          modalShow={modalShow}
+          closeModalHandler={closeModalHandler}
+          loginHandler={loginHandler}
+        />
+        {Object.keys(dashboard.columns).map((key) => {
+          const column: { [index: string]: any } = dashboard.columns[key];
+          const { id, name, cards } = column;
+          return (
+            <CardsListContainer
+              cards={cards}
+              key={id}
+              name={name}
+              columnId={id}
+            />
+          );
+        })}
+      </>
     </section>
   );
 }
