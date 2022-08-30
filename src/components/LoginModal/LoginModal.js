b520c04d943d668f6f37ef563415656c7fcb9e4e
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-08-31 05:16:48 +0600

    add signInUser request, restructure and restyle

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -2,41 +2,58 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './style.css';
+import { signInUser } from '../../api';
+import { useState } from 'react';
 
-const LoginModal = ({ modalShow, closeModalHandler, loginHandler }) => {
+const LoginModal = ({ modalShow, closeModalHandler }) => {
+  const [validated, setValidated] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+    } else {
+      signInUser(inputValue);
+      closeModalHandler();
+    }
+    setValidated(true);
+  };
+
   return (
     <Modal
       show={modalShow}
-      onHide={closeModalHandler}
+      //   onHide={keepModalOpen}
       size='lg'
       aria-labelledby='contained-modal-title-vcenter'
       centered
     >
-      <Modal.Header closeButton>
+      {/* <Modal.Header closeButton>
         <Modal.Title>Please enter your name to sign in</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
+      </Modal.Header> */}
+      <Modal.Body className='sign-in-modal'>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-            <Form.Label>Name</Form.Label>
-            <Form.Control type='text' autoFocus />
+            <Form.Label>Please enter your name to sign in</Form.Label>
+            <Form.Control
+              type='text'
+              name='name'
+              autoFocus
+              required
+              placeholder='Test users: John, Mary, Chris, Mike'
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+            />
           </Form.Group>
+          <Button type='submit' variant='primary'>
+            Sign In
+          </Button>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant='secondary' onClick={closeModalHandler}>
-          Close
-        </Button>
-        <Button
-          variant='primary'
-          onClick={() => {
-            loginHandler();
-            closeModalHandler();
-          }}
-        >
-          Sign In
-        </Button>
-      </Modal.Footer>
+      {/* <Modal.Footer></Modal.Footer> */}
     </Modal>
   );
 };
