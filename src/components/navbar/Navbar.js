09735212a25f81866a63badb18e545ab21c8dd22
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-02 04:30:13 +0600

    update Navbar to use app-context and more

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,23 @@
+import { Button } from 'react-bootstrap';
 import BootstrapContainer from 'react-bootstrap/Container';
 import BootstrapNavbar from 'react-bootstrap/Navbar';
+import { useAppContext } from '../../context/app-context';
 import './style.css';
 
-const Navbar = ({ currentUser }) => {
+const Navbar = ({ logout }) => {
+  const { currentUser } = useAppContext();
   return (
     <BootstrapNavbar>
       <BootstrapContainer>
-        <BootstrapNavbar.Brand href='#home'>Dashboard</BootstrapNavbar.Brand>
+        <BootstrapNavbar.Brand href='#home'>Kanban Board</BootstrapNavbar.Brand>
         <BootstrapNavbar.Toggle />
         <BootstrapNavbar.Collapse className='justify-content-end'>
           <BootstrapNavbar.Text>
-            Signed in as: {currentUser}
+            Signed in as:{' '}
+            <span className='current-user-name'>{currentUser}</span> |{' '}
+            <span className='logout' onClick={logout}>
+              Sign out
+            </span>
           </BootstrapNavbar.Text>
         </BootstrapNavbar.Collapse>
       </BootstrapContainer>
