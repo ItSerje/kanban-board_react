0d6a1364af99f90fe447ab9a6f407251f1c12178
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-02 04:19:26 +0600

    add fetchById api request

--- src/api.js
+++ src/api.js
@@ -27,4 +27,18 @@ const fetchDashboard = async () => {
   return null;
 };
 
-export { fetchDashboard };
+const fetchCardById = async (id) => {
+  await asyncTimeout(RESPONSE_DELAY);
+  const data = localStorage.getItem('db');
+  if (data) {
+    const parsedData = await JSON.parse(data);
+    const cards = parsedData.dashboard.columns
+      .map((column) => column.cards) // add find here, then get value from the array which includes undefined values
+      .flat();
+    const card = cards.find((card) => card.id === id);
+    return card;
+  }
+  return null;
+};
+
+export { fetchDashboard, fetchCardById };
