f0e878dd483cf4aad2c4a4dfbd00d6b591e0d255
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-02 17:56:26 +0600

    enable update and delete card handlers

--- src/components/cardForm/FullCardContainer.js
+++ src/components/cardForm/FullCardContainer.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { fetchCardById } from '../../api';
+import { fetchCardById, updateCard, deleteCard } from '../../api';
 import FullCard from './FullCard';
 import Spinner from 'react-bootstrap/Spinner';
 import { useAppContext } from '../../context/app-context';
@@ -7,27 +7,32 @@ import { useAppContext } from '../../context/app-context';
 const FullCardContainer = ({ cardId, closeModal }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [card, setCard] = useState(null);
+  const [refreshCard, setRefreshCard] = useState(0);
 
   const { refreshDashboard } = useAppContext();
 
-  const updateCardHandler = (updatedCard) => {
-    // updateCard() // api
-    // refreshDashboard();
+  const updateCardHandler = async (inputName, updatedValue) => {
+    const updatedCard = { ...card, [inputName]: updatedValue.trim() };
+    await updateCard(updatedCard);
+    setRefreshCard(refreshCard + 1);
+    await refreshDashboard();
   };
 
-  const deleteCardHandler = (updatedCard) => {
-    // updateCard() // api
-    // refreshDashboard();
+  const deleteCardHandler = async (id) => {
+    await deleteCard(id);
+    closeModal();
+    await refreshDashboard();
   };
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const card = await fetchCardById(cardId);
       setCard(card);
       setIsLoading(false);
     };
     fetchData();
-  }, [cardId]);
+  }, [cardId, refreshCard]);
 
   if (isLoading) {
     return <Spinner animation='border' variant='primary' className='spinner' />;
