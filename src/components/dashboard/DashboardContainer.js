12b8d1e2ec5b9c42b63d303969abd2d0ee4e7685
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-03 14:33:23 +0600

    add isLoading spinner to dashboard

--- src/components/dashboard/DashboardContainer.js
+++ src/components/dashboard/DashboardContainer.js
@@ -2,19 +2,28 @@ import { useState, useEffect } from 'react';
 import { fetchDashboard } from '../../api';
 import Dashboard from './Dashboard';
 import { useAppContext } from '../../context/app-context';
+import Spinner from 'react-bootstrap/Spinner';
 import './style.css';
 
 const DashboardContainer = () => {
   let { refreshDashboardTrigger } = useAppContext();
   const [dashboard, setDashboard] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const dashboard = await fetchDashboard();
       setDashboard(dashboard);
+      setIsLoading(false);
     };
+
     fetchData();
   }, [refreshDashboardTrigger]);
+
+  if (isLoading) {
+    return <Spinner animation='border' variant='primary' className='spinner' />;
+  }
 
   return <Dashboard dashboard={dashboard} />;
 };
