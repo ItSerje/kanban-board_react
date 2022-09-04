d0db67fc12081fab05e608d3d12b58169613f520
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-05 00:38:05 +0600

    delete comments

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -37,23 +37,11 @@ const Card = ({ card, columnName }) => {
         onMouseEnter={() => setIsIconShown(true)}
         onMouseLeave={() => setIsIconShown(false)}
       >
-        {/* <Card.Img variant='top' src='holder.js/100px180' /> */}
         <FaRegEdit className={isIconShown ? 'edit-icon' : 'edit-icon hidden'} />
-
         <BootstrapCard.Body className='card-list-card-body'>
           <BootstrapCard.Title as='div'>{title}</BootstrapCard.Title>
-          {/* <BootstrapCard.Text>{author}</BootstrapCard.Text> */}
-          {/* <Button variant='primary' onClick={addCardHandler}>
-          Add card
-        </Button> */}
         </BootstrapCard.Body>
-
         <BootstrapCard.Footer>
-          {/* <small className='text-muted'>Last updated 3 mins ago</small> */}
-
-          {/* maybe the below is worth to be a separate component? */}
-          {/* basides there is an option to use react-bootstrap Badge and/or ListGroup */}
-
           <div className='card-badge' title='Comments'>
             <span className='card-badge-icon'></span>
             <span className='card-badge-text'>{comments.length}</span>
