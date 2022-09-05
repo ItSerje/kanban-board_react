4103bb8e4d94000787ba317d993315d086336b64
ItSerje <66359825+ItSerje@users.noreply.github.com>
2022-09-05 17:52:28 +0600

    remove comments

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,29 +1,3 @@
-// // import React, { useEffect, useRef } from 'react';
-
-// const useOutsideClick = (
-//   callback: () => void
-// ): [React.MutableRefObject<HTMLElement | null | undefined>] => {
-//   const ref = useRef<HTMLDivElement>(null!);
-
-//   useEffect(() => {
-//     const handleClick = ({ target }: MouseEvent): void => {
-//       if (ref.current && !ref.current.contains(target as Node)) {
-//         callback();
-//       }
-//     };
-
-//     document.addEventListener('click', handleClick, true);
-
-//     return () => {
-//       document.removeEventListener('click', handleClick, true);
-//     };
-//   }, [ref, callback]);
-
-//   return ref;
-// };
-
-// export default useOutsideClick;
-
 import { useEffect, RefObject } from 'react';
 
 type Event = MouseEvent | TouchEvent;
@@ -39,18 +13,14 @@ const useOutsideClick = <T extends HTMLElement = HTMLElement>(
         return;
       }
 
-      handler(event); // Call the handler only if the click is outside of the element passed.
+      handler(event);
     };
     document.addEventListener('click', listener, true);
-    // document.addEventListener('mousedown', listener);
-    // document.addEventListener('touchstart', listener);
 
     return () => {
       document.removeEventListener('click', listener, true);
-      //   document.removeEventListener('mousedown', listener);
-      //   document.removeEventListener('touchstart', listener);
     };
-  }, [ref, handler]); // Reload only if ref or handler changes
+  }, [ref, handler]);
 };
 
 export default useOutsideClick;
